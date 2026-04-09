f98dfde31276c87dc75b0ec69dcde2c72bbd7fe2
mayuri-vadi <developer7.intelliworkz@gmail.com>
2026-04-09 19:25:49 +0530

    Date: 09-04-2026
    - Added more packages in list.

--- lib/kit_data.dart
+++ lib/kit_data.dart
@@ -1,6 +1,6 @@
 library flutter_core_kit;
 
+export 'core/services/internet/ck_internet_service.dart';
 // core.core.utils
 export 'core/utils/currency_formatter.dart';
-
 export 'core/utils/parser_utils.dart';

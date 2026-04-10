448dd6b3077e5238ab7a2d82842661a849f715f1
mayuri-vadi <developer7.intelliworkz@gmail.com>
2026-04-10 19:04:55 +0530

    Date: 10-04-2026
    - Added more packages in list.

--- lib/kit_data.dart
+++ lib/kit_data.dart
@@ -17,10 +17,12 @@ export 'core/utils/parser_utils.dart';
 export 'core/utils/validators.dart';
 export 'core/widgets/ImagePicker/ck_image_picker_provider.dart';
 export 'core/widgets/ImagePicker/ck_image_picker_service.dart';
-export 'core/widgets/ck_elevated_button.dart';
+export 'core/widgets/button/ck_elevated_button.dart';
 export 'core/widgets/date/ck_date_field.dart';
 export 'core/widgets/date/ck_date_time_field.dart';
 export 'core/widgets/date/ck_time_field.dart';
+export 'core/widgets/dropdown/ck_api_dropdown.dart';
+export 'core/widgets/dropdown/ck_static_dropdown.dart';
 // ─── Widgets ───────────────────────────────────────────────
 export 'core/widgets/image/ck_cache_network_image.dart';
 export 'core/widgets/loading/ck_loading_overlay.dart';
